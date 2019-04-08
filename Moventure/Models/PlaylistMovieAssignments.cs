fcd6e4b3f72a361cf353d821b77bd71875c6803f
ValentinHorga <valentinhorga@gmail.com>
2019-04-08 18:57:21 +0300

    Initialized project structure

---
+++ Moventure/Models/PlaylistMovieAssignments.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Moventure.WebApi.Models
+{
+    public partial class PlaylistMovieAssignments
+    {
+        public Guid PlaylistId { get; set; }
+        public Guid MovieId { get; set; }
+
+        public Movies Movie { get; set; }
+    }
+}
