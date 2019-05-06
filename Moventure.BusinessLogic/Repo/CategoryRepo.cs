8f0d49663c492d303e5676cd2f2941b7a9cebb09
Flavius-Radu Demian <flavius.demian@deventure.co>
2019-05-06 19:42:04 +0300

    added BaseRepository sample implementation

--- Moventure.BusinessLogic/Repo/CategoryRepo.cs
+++ Moventure.BusinessLogic/Repo/CategoryRepo.cs
@@ -6,5 +6,27 @@ namespace Moventure.BusinessLogic.Repo
 {
     public class CategoryRepo : BaseSinglePkRepository<Categories>
     {
+        /*
+         *         protected override AspNetRole FetchFromDb(AspNetRole entity, IList<string> navigationProperties = null)
+        {
+            return GetSingle(aspNetRole => aspNetRole.Id == entity.Id, navigationProperties);
+        }
+
+        protected override async Task<AspNetRole> FetchFromDbAsync(AspNetRole entity, IList<string> navigationProperties = null)
+        {
+            return await GetSingleAsync(aspNetRole => aspNetRole.Id == entity.Id, navigationProperties).ConfigureAwait(false);
+        }
+
+        protected override bool ValidateEntity(AspNetRole entity)
+        {
+            if (string.IsNullOrEmpty(entity?.Id) && entity.Id != Guid.Empty.ToString())
+            {
+                return true;
+            }
+
+            LogHelper.LogException<BaseDataRepository>("Invalid entity: null or empty primaty keys");
+            return false;
+        }
+         */
     }
 }
