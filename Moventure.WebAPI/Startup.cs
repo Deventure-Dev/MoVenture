9ef267c2c4e820c577736453bd3fc4c4f3f0256b
Petricele <petricelealexandru@yahoo.com>
2019-05-13 19:38:08 +0300

    added knockout.js and jquery ...

--- Moventure.WebAPI/Startup.cs
+++ Moventure.WebAPI/Startup.cs
@@ -27,6 +27,8 @@ namespace Moventure.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+
             //CustomDesignTimeServices.ConfigureDesignTimeServices(services);
 
             //configure authorization
