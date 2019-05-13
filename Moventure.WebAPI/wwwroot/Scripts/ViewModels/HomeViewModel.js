10e8f2fe4ee90079862b7b4cd79053fcfea2393a
Petricele <petricelealexandru@yahoo.com>
2019-05-13 17:28:03 +0300

    added ViewModels: Category, Home ,Minified

---
+++ Moventure.WebAPI/wwwroot/Scripts/ViewModels/HomeViewModel.js
@@ -0,0 +1,19 @@
+﻿var HomeViewModel = function () {
+    var self = this;
+
+
+    self.Categories = ko.observableArray();
+
+    self.init = function (categories) {
+        var categoriesList = [];
+        for (var i = 0; i < categories.length; i++)
+        {
+            var categoryVM = new CategoryViewModel();
+            var currentCategory = categories[i];
+            categoryVM.init(currentCategory.Id, currentCategory.Name,
+                            currentCategory.SavedAt, currentCategory.SavedBy);
+            categoriesList.push(categoryVM);
+        }
+        self.Categories(categoriesList);
+    }
+}
