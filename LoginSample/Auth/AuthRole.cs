0bafce37deff968c337b6cf80086421be7abb22f
Flavius-Radu Demian <flavius.demian@deventure.co>
2019-05-26 20:29:19 +0300

    removed files with sensitive data

---
+++ LoginSample/Auth/AuthRole.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace LoginSample.Auth
+{
+    public class AuthRole : IdentityRole<int>
+    {
+        public ICollection<UserRole> UserRoles { get; set; }
+    }
+}
---
+++ LoginSample/Auth/UserRole.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginSample.Auth
+{
+    public class UserRole : IdentityUserRole<int>
+    {
+        public AuthUser User { get; set; }
+        public AuthRole Role { get; set; }
+    }
+}
