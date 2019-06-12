72122449aa164333fd3658cb603953b38ca8578c
Bogdan Popovici <bogdan.popovici@deventure.co>
2019-06-12 17:34:32 +0300

    fix details movie

--- Moventure.WebAPI/wwwroot/Scripts/ViewModels/MovieDetailsViewModel.js
+++ Moventure.WebAPI/wwwroot/Scripts/ViewModels/MovieDetailsViewModel.js
@@ -9,6 +9,7 @@
     self.Actors = ko.observableArray();
 
     self.init = function (id, title, pictureUrl, rating, tags, actors, categoryName) {
+        debugger
         self.Id(id);
         self.Title(title);
         self.PictureUrl(pictureUrl);
@@ -25,6 +26,7 @@ var MovieDetailsViewModel = function () {
     self.FetchedMovie = ko.observable();
 
     self.init = function (data) {
+        debugger
         var currentMovie = new CurrentMovieViewModel();
         currentMovie.init(data[0].id, data[0].title, data[0].pictureUrl, data[0].rating, data[0].tags, data[0].actors, data[0].categoryName)
         self.FetchedMovie(currentMovie);
---
+++ Moventure.WebAPI/wwwroot/Scripts/ViewModels/mainViewModel.js
@@ -0,0 +1,47 @@
+﻿// app.js
+(function ($) {
+    var app = Sammy("body");
+    app.use(Sammy.Template, "html");
+
+    $(document).ready(function () {
+        app.run("#/");
+    });
+})(jQuery);
+
+// controller.home
+(function () {
+    var app = Sammy.apps.body;
+    debugger
+    app.get("#/", homeController);
+    app.get("#/home", homeController);
+
+    function homeController(context) {
+        context.render($("#home_html"), {}, homeView);
+    };
+
+    function homeView(view) {
+        $("#view").html(view);
+      
+    };
+})();
+
+
+// controller.home
+(function () {
+    var app = Sammy.apps.body;
+    app.get("#/details/", homeController);
+
+    function homeController(context) {
+        context.render($("#details"), {}, homeView);
+    };
+
+    function homeView(view) {
+        $("#view").html(view);
+      
+    };
+})();
+
+
+
+(function () {
+})(window);
